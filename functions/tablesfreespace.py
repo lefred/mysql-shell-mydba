5102e84a61d44e3999a263e1d87f08b2c3d1d0b0
Frederic Descamps <lefred.descamps@gmail.com>
2018-12-17 14:36:14 +0100

    add warning
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- functions/tablesfreespace.py
+++ functions/tablesfreespace.py
@@ -9,23 +9,37 @@ def getFragmentedTables(percentage=10):
         print("Please create a connection first.")
         return False
     session=globals.session
+    stmt = "select variable_value from performance_schema.global_variables where variable_name='information_schema_stats_expiry';"
+    query = session.sql(stmt)
+    result = query.execute()
+    stats = result.fetch_all()
+    if len(stats) > 0:
+       for stat in stats:
+           if stat > 0:
+               print "Warning: information_schema_stats_expiry is set to {0}.".format(*stat)
+
+
     stmt = """SELECT CONCAT(table_schema, '.', table_name) as 'TABLE', 
        ENGINE, CONCAT(ROUND(table_rows / 1000000, 2), 'M') `ROWS`, 
        CONCAT(ROUND(data_length / ( 1024 * 1024 * 1024 ), 2), 'G') DATA, 
        CONCAT(ROUND(index_length / ( 1024 * 1024 * 1024 ), 2), 'G') IDX, 
        CONCAT(ROUND(( data_length + index_length ) / ( 1024 * 1024 * 1024 ), 2), 'G') 'TOTAL SIZE', 
-       ROUND(index_length / data_length, 2)  IDXFRAC, CONCAT(ROUND(( data_free / 1024 / 1024),2), 'MB') AS data_free,
-       concat(round(data_free/(data_length+index_length)*100,2),'%') as data_free_pct
+       ROUND(index_length / data_length, 2)  IDXFRAC, 
+        CONCAT(ROUND(( data_free / 1024 / 1024),2), 'MB') AS data_free,
+       CONCAT('(',
+        IF(data_free< ( data_length + index_length ), 
+        CONCAT(round(data_free/(data_length+index_length)*100,2),'%'),
+       '100%'),')') AS data_free_pct
        FROM information_schema.TABLES  WHERE (data_free/(data_length+index_length)*100) > {limit}
        AND table_schema <> 'mysql';""".format(limit=percentage) 
     query = session.sql(stmt)
     result = query.execute()
     tables = result.fetch_all()
-    fmt_h = "| {0:30} | {1:10} | {2:7} | {3:10} | {4:10} | {5:10} | {6:16} |"
-    fmt = "| {0:30} | {1:10} | {2:>7} | {3:>10} | {4:>10} | {5:>10} | {7:>7} ({8:6}) |"
+    fmt_h = "| {0:30} | {1:10} | {2:7} | {3:10} | {4:10} | {5:10} | {6:17} |"
+    fmt = "| {0:30} | {1:10} | {2:>7} | {3:>10} | {4:>10} | {5:>10} | {7:>7} {8:8} |"
     header = fmt_h.format("Table","Engine","# Rows", "Data Size", "Idx Size", "Total Size", "Data Free")
     bar = "+" + "-" * 32 + "+" + "-" * 12 + "+" + "-" * 9 + "+" + "-" * 12 + \
-          "+" + "-" * 12 + "+" + "-" * 12 + "+" + "-" * 18 + "+"
+          "+" + "-" * 12 + "+" + "-" * 12 + "+" + "-" * 19 + "+"
     if len(tables) > 0:
         print bar
         print header
